c9269d1f3e75a04db007b37135411f7b4ea92812
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2024-06-25 01:01:15 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PythonTools",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PythonTools",
+            targets: ["PythonTools"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PythonTools"),
+        .testTarget(
+            name: "PythonToolsTests",
+            dependencies: ["PythonTools"]
+        ),
+    ]
+)
