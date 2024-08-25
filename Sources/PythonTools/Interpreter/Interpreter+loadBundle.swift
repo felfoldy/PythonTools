a7ae1ee6b5dc19d5fb92ad18a8a4e7f920ed9dc7
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2024-08-25 17:53:26 +0200

    Update bundleLoad

--- Sources/PythonTools/Interpreter/Interpreter+loadBundle.swift
+++ Sources/PythonTools/Interpreter/Interpreter+loadBundle.swift
@@ -10,6 +10,7 @@ import PythonKit
 
  extension Interpreter {
      /// Should access in python thread only.
+     @MainActor
      static var loadedModules = Set<String>()
      
      /// Load a bundle if it isn't loaded already.
@@ -19,21 +20,26 @@ import PythonKit
      ///   - bundle: bundle to load
      ///   - loaded: executed after the bundle is loaded on the python thread.
      public static func load(bundle: Bundle, loaded: (() throws -> Void)? = nil) async throws {
-         try await perform {
-             guard let identifier = bundle.bundleIdentifier,
-                   !loadedModules.contains(identifier) else {
-                 return
-             }
-             
+         guard let identifier = bundle.bundleIdentifier else {
+             throw InterpreterError.failedToLoadBundle
+         }
+         
+         var isLoaded = false
+         
+         try await performOnMain {
+             isLoaded = loadedModules.contains(identifier)
              loadedModules.insert(identifier)
-             
-             bundle.load()
-             
-             guard let path = bundle.path(forResource: "site-packages", ofType: nil) else {
-                 throw InterpreterError.failedToLoadBundle
-             }
-             
-             
+         }
+         
+         if isLoaded { return }
+         
+         bundle.load()
+
+         guard let path = bundle.path(forResource: "site-packages", ofType: nil) else {
+             throw InterpreterError.failedToLoadBundle
+         }
+         
+         try await perform {
              let sys = Python.import("sys")
              sys.path.append(path)
              
